36343d2cc8edf9d7c78b1e17bef7b6cfe4bc2017
justyna_eevee <justyna.zaczyk@onet.pl>
2021-06-28 17:57:17 +0200

    level generation

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    public GameObject[] TerrainBlocks;  // miejsce na wszystkie poziomy
+
+    private int BlockIndex = 0; // funkcja generująca od 0 do nieskończoności 
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GenerateBlock();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void GenerateBlock()// funkcja odpowiedzialna za umieszczanie jednego losowego bloku  
+    {
+        var prefab = TerrainBlocks[0]; // losowy element
+        var block = Instantiate(prefab); // instancja prefabu
+        // umieszczenie bloku na jednej pozycji
+        block.transform.position = Vector2.right * BlockIndex * 8f; ; // ustawianie prefaba na odpowiedniej pozycji
+        BlockIndex++; 
+    }
+}
